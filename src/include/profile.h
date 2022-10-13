278fe9bee6da3a38f0572b9c44a223a37582cdc5
Kelton Halbert <kelton.halbert@noaa.gov>
2022-10-13 20:58:36 +0000

    added authorship, license, date, origins, etc

--- src/include/profile.h
+++ src/include/profile.h
@@ -1,4 +1,13 @@
+// Author: Kelton Halbert
+// Email: kelton.halbert@noaa.gov
+// License: Apache 2.0
+// Date: 2022-10-13
+//
+// Written for the NWS Storm Predidiction Center
+// Based on NSHARP routines originally written by
+// John Hart and Rich Thompson at SPC. 
 #pragma once
+
 namespace sharp {
     
 
