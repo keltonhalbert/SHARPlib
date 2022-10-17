de93379f6bad8ece14fd17e405e84950828e6488
Kelton Halbert <kthalbert@wxbyte.com>
2022-10-17 13:40:12 -0500

    added a constants file

---
+++ src/include/constants.h
@@ -0,0 +1,24 @@
+// Author: Kelton Halbert
+// Email: kelton.halbert@noaa.gov
+// License: Apache 2.0
+// Date: 2022-10-13
+//
+// Written for the NWS Storm Predidiction Center
+// Based on NSHARP routines originally written by
+// John Hart and Rich Thompson at SPC. 
+#pragma once 
+
+namespace sharp {
+
+	static const float ZEROCNK = 273.15f; // conversion between celsius and kelvin
+	static const float MISSING = -9999.0f; // missing array values
+	static const float GRAVITY = 9.80665f; // Gravitational constant in m/s^2
+	static const float TOL = 1e-10; // Floating point tolerance for iterative convergence functions
+	static const float PI = 3.14159265; // You know... Pi...
+	static const float RDGAS = 287.052874; // Dry air gas constant, J/(kg*K)
+	static const float RVGAS = 461.52f; // Water vapor gas constant, J/(kg*K)
+	static const float CP_DRYAIR = 1005.7f; // specific heat capacity of dry air J/kg
+	static const float CP_WATER = 4184.f; // specific heat capacity of water J/kg
+	static const float LV = 2.5104e6f; // Latent heat of vaporization of water (J/kg)
+	
+}
