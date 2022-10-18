6519b3ea3a4ab930bbba53a2029e91167c7e0499
Kelton Halbert <kelton.halbert@noaa.gov>
2022-10-18 19:30:30 +0000

    Added Doxygen to the project. Moved documentation to header files and reformatted appropriately to use Doxygen syntax

--- src/include/constants.h
+++ src/include/constants.h
@@ -1,11 +1,16 @@
-// Author: Kelton Halbert
-// Email: kelton.halbert@noaa.gov
-// License: Apache 2.0
-// Date: 2022-10-13
-//
-// Written for the NWS Storm Predidiction Center
-// Based on NSHARP routines originally written by
-// John Hart and Rich Thompson at SPC. 
+/**
+ * \file
+ * \brief Meteorological constants useful for computations with vertical atmospheric sounding profiles 
+ * \author  
+ *   Kelton Halbert                  \n
+ *   Email: kelton.halbert@noaa.gov  \n
+ *   License: Apache 2.0             \n
+ * \date   2022-10-13
+ *
+ * Written for the NWS Storm Predidiction Center \n
+ * Based on NSHARP routines originally written by
+ * John Hart and Rich Thompson at SPC. 
+ */
 #pragma once 
 
 namespace sharp {
@@ -20,5 +25,5 @@ namespace sharp {
 	static const float CP_DRYAIR = 1005.7f; // specific heat capacity of dry air J/kg
 	static const float CP_WATER = 4184.f; // specific heat capacity of water J/kg
 	static const float LV = 2.5104e6f; // Latent heat of vaporization of water (J/kg)
-	
+	static const float ROCP = RDGAS / CP_DRYAIR;
 }
