8a238487b7ae377a6948c755d2b259a2b10e9f40
Kelton Halbert <kelton.halbert@noaa.gov>
2022-10-25 21:09:46 +0000

    Took a style pass at some variable names, added a few comments, and made constants compile-time constants

--- include/constants.h
+++ include/constants.h
@@ -15,15 +15,15 @@
 
 namespace sharp {
 
-	static const float ZEROCNK = 273.15f; // conversion between celsius and kelvin
-	static const float MISSING = -9999.0f; // missing array values
-	static const float GRAVITY = 9.80665f; // Gravitational constant in m/s^2
-	static const float TOL = 1e-10; // Floating point tolerance for iterative convergence functions
-	static const float PI = 3.14159265; // You know... Pi...
-	static const float RDGAS = 287.052874; // Dry air gas constant, J/(kg*K)
-	static const float RVGAS = 461.52f; // Water vapor gas constant, J/(kg*K)
-	static const float CP_DRYAIR = 1005.7f; // specific heat capacity of dry air J/kg
-	static const float CP_WATER = 4184.f; // specific heat capacity of water J/kg
-	static const float LV = 2.5104e6f; // Latent heat of vaporization of water (J/kg)
-	static const float ROCP = RDGAS / CP_DRYAIR;
+	static constexpr float ZEROCNK = 273.15f; // conversion between celsius and kelvin
+	static constexpr float MISSING = -9999.0f; // missing array values
+	static constexpr float GRAVITY = 9.80665f; // Gravitational constant in m/s^2
+	static constexpr float TOL = 1e-10; // Floating point tolerance for iterative convergence functions
+	static constexpr float PI = 3.14159265; // You know... Pi...
+	static constexpr float RDGAS = 287.052874; // Dry air gas constant, J/(kg*K)
+	static constexpr float RVGAS = 461.52f; // Water vapor gas constant, J/(kg*K)
+	static constexpr float CP_DRYAIR = 1005.7f; // specific heat capacity of dry air J/kg
+	static constexpr float CP_WATER = 4184.f; // specific heat capacity of water J/kg
+	static constexpr float LV = 2.5104e6f; // Latent heat of vaporization of water (J/kg)
+	static constexpr float ROCP = RDGAS / CP_DRYAIR;
 }
