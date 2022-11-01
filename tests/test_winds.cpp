e5c9128514b23af13dcd33a32d0953c8ee82410d
Kelton Halbert <kelton.halbert@noaa.gov>
2022-11-01 21:19:21 +0000

    added testing files and build instructions for kinematic routines, but have not implemented tests yet

---
+++ tests/test_winds.cpp
@@ -0,0 +1,12 @@
+#define DOCTEST_CONFIG_IMPLEMENT_WITH_MAIN
+#include <cmath>
+#include <limits>
+
+#include "constants.h"
+#include "doctest.h"
+#include "winds.h"
+
+TEST_CASE("Testing vector -> components operations") {
+
+
+}
