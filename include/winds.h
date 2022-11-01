5fda327b4fa422a29f411c14ea20e433386b6c8e
Kelton Halbert <kelton.halbert@noaa.gov>
2022-11-01 20:47:05 +0000

    initial implementation of kinematic routines

--- include/winds.h
+++ include/winds.h
@@ -15,6 +15,168 @@
 
 namespace sharp {
 
+/**
+ *
+ * \author Kelton Halbert - NWS Storm Prediction Center/OU-CIWRO
+ *
+ * \brief A simple structure of two named floats that represent a wind vector.
+ *
+ * This is used in the convenience function sharp::components_to_vector
+ * for situations where having both the speed and direction are desired
+ * from components with a single function call. 
+ */
+struct WindVector {
+    /**
+     * \brief The magntidue of a wind vector
+     */ 
+    float speed;
+
+    /**
+     * \brief The direction (degrees from North) of a wind vector.  
+     */ 
+    float direction;
+};
+
+/**
+ *
+ * \author Kelton Halbert - NWS Storm Prediction Center/OU-CIWRO
+ *
+ * \brief A simple structure of two names floats that represent the components of a wind vector. 
+ *
+ * This is used in the convenience function sharp::vector_to_components
+ * for situations where having both the U and V wind components are 
+ * desired from a wind vector with a single function call. 
+ *
+ */
+struct WindComponents {
+    /**
+     * \brief The zonal (U) component of a wind vector.  
+     */ 
+    float u;
+    /**
+     * \brief The meridional (V) component of a wind vector.  
+     */ 
+    float v;
+};
+
+
+/**
+ *
+ * \author John Hart - NSSFC KCMO / NWSSPC OUN
+ *
+ * \brief Computes the zonal (U) wind component from a wind vector. 
+ *
+ * Given the wind speed and direction, compute and return the zonal 
+ * (U) wind component.
+ *
+ * \param wind_speed     (distance / time)
+ * \param wind_direction (degrees from North)
+ * \return u_component   (distance / time)
+ */
+float u_component(float wind_speed, float wind_direction);
+
+
+/**
+ *
+ * \author John Hart - NSSFC KCMO / NWSSPC OUN
+ *
+ * \brief Computes the meridional (V) wind component from a wind vector. 
+ *
+ * Given the wind speed and direction, compute and return the meridional
+ * (V) wind component.
+ *
+ * \param wind_speed     (distance / time)
+ * \param wind_direction (degrees from North)
+ * \return v_component   (distance / time)
+ */
+float v_component(float wind_speed, float wind_direction);
+
+
+/**
+ *
+ * \author John Hart - NSSFC KCMO / NWSSPC OUN
+ *
+ * \brief Computes the direction (degrees from North) a vector points to. 
+ *
+ * Given the zonal (U) and meridional (V) components of a vector, compute
+ * and return the direction (degrees from North) the vector points to. 
+ *
+ * \param u_comp            (distance / time)
+ * \param v_comp            (distance / time)
+ * \return wind_direction   (degrees from North)
+ */
+float vector_angle(float u_comp, float v_comp);
+
+
+/**
+ *
+ * \author John Hart - NSSFC KCMO / NWSSPC OUN
+ *
+ * \brief Computes the magnitude of a vector given its components. 
+ *
+ * Given the zonal (U) and meridional (V) components of a vector, compute
+ * and return the magnitude (distance / time) of the vector.  
+ *
+ * \param u_comp        (distance / time)
+ * \param v_comp        (distance / time)
+ * \return wind_speed   (distance / time)
+ */
+float vector_magnitude(float u_comp, float v_comp);
+
+
+/**
+ *
+ * \author John Hart - NSSFC KCMO / NWSSPC OUN
+ *
+ * \brief Precisely computes the magnitude of a vector given its components. 
+ *
+ * Given the zonal (U) and meridional (V) components of a vector, compute
+ * and return the magnitude (distance / time) of the vector. Instead of using
+ * a standard sqrt(u*u + v*v) approach, this uses the c++ Standard Template 
+ * Library implementation of std::hypot. Using std::hypot is ~20 times slower,
+ * but ensures a higher accuracy and checks for overflow/underflow when using
+ * extremely large or extremely small values. Chances are, you are fine using 
+ * the imprecise version as long as values are in the range of 
+ * 1e-100 < values < 1e100.  
+ *
+ * \param u_comp        (distance / time)
+ * \param v_comp        (distance / time)
+ * \return wind_speed   (distance / time)
+ */
+float vector_magnitude_precise(float u_comp, float v_comp);
+
+
+/**
+ *
+ * \author Kelton Halbert - NWS Storm Prediction Center/OU-CIWRO
+ *
+ * \brief Conveniance function to compute wind speed and direction from components 
+ *
+ * Given the zonal (U) and meridional (V) components of a vector, compute
+ * and return the wind speed (distance / time) and direction (degrees from North)
+ * from the components as a struct. 
+ *
+ * \param u_comp                (distance / time)
+ * \param v_comp                (distance / time)
+ * \return sharp::WindVector    {wind_speed, wind_direction}
+ */
+WindVector components_to_vector(float u_comp, float v_comp);
+
+
+/**
+ *
+ * \author Kelton Halbert - NWS Storm Prediction Center/OU-CIWRO
+ *
+ * \brief Conveniance function to compute the U and V components of a wind vector. 
+ *
+ * Given the wind speed and direction of a vector, compute and return the 
+ * zonal and meridional vector components as a struct. 
+ *
+ * \param wind_speed                (distance / time)
+ * \param wind_direction            (degrees from North)
+ * \return sharp::WindComponents    {u_comp, v_comp}
+ */
+WindComponents vector_to_components(float wind_speed, float wind_direction);
 
 
 }
--- src/sharptab/winds.cpp
+++ src/sharptab/winds.cpp
@@ -6,9 +6,86 @@
 // Written for the NWS Storm Predidiction Center
 // Based on NSHARP routines originally written by
 // John Hart and Rich Thompson at SPC. 
+#include <cmath>
+
+#include "constants.h"
+#include "winds.h"
 
 namespace sharp {
 
+
+float u_component(float wind_speed, float wind_direction) {
+#ifndef NO_QC
+    if ((wind_direction == MISSING) || (wind_speed == MISSING))
+        return MISSING;
+#endif
+    wind_direction *= (PI / 180.0);
+    return -1.0 * wind_speed * std::sin(wind_direction);
+}
+
+
+float v_component(float wind_speed, float wind_direction) {
+#ifndef NO_QC
+    if ((wind_direction == MISSING) || (wind_speed == MISSING))
+        return MISSING;
+#endif
+    wind_direction *= (PI / 180.0);
+    return -1.0 * wind_speed * std::cos(wind_direction);
+}
+
+
+float vector_angle(float u_comp, float v_comp) {
+#ifndef NO_QC
+    if ((u_comp == MISSING) || (v_comp == MISSING))
+        return MISSING;
+#endif
+    if ((u_comp == 0) && (v_comp == 0))
+        return 0;
+
+    float wind_direction = std::atan2( -1*v_comp, -1*u_comp) * (180.0 / PI);
+    if (wind_direction < 0) wind_direction += 360.0;
+    if (wind_direction < TOL) wind_direction = 0;
+    return wind_direction; 
+}
+
+
+float vector_magnitude(float u_comp, float v_comp) {
+#ifndef NO_QC
+    if ((u_comp == MISSING) || (v_comp == MISSING))
+        return MISSING;
+#endif
+    return std::sqrt((u_comp * u_comp) + (v_comp * v_comp));
+}
+
+
+float vector_magnitude_precise(float u_comp, float v_comp) {
+#ifndef NO_QC
+    if ((u_comp == MISSING) || (v_comp == MISSING))
+        return MISSING;
+#endif
+    return std::hypot(u_comp, v_comp);
+}
+
+
+WindVector components_to_vector(float u_comp, float v_comp) {
+    float wind_speed = vector_magnitude(u_comp, v_comp);
+    float wind_direction = vector_angle(u_comp, v_comp);
+
+    return {wind_speed, wind_direction};
+}
+
+
+WindComponents vector_to_components(float wind_speed, float wind_direction) {
+#ifndef NO_QC
+    if ((wind_direction == MISSING) || (wind_speed == MISSING))
+        return {MISSING, MISSING}; 
+#endif
+    wind_direction *= (PI / 180.0);
+    float u_comp = -1.0 * wind_speed * std::sin(wind_direction);
+    float v_comp = -1.0 * wind_speed * std::cos(wind_direction);
+
+    return {u_comp, v_comp};
+}
 
 
 }
