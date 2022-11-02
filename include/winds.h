47698d486ea31f3af43c50a58406ee262e527390
Kelton Halbert <kelton.halbert@noaa.gov>
2022-11-02 21:51:39 +0000

    added wind shear

--- include/winds.h
+++ include/winds.h
@@ -302,6 +302,27 @@ WindComponents mean_wind_npw(float pressure_bot,  float pressure_top,
                              const float *pres,   const float* u_wind,
                              const float *v_wind, int num_levs);
 
+/**
+ *
+ * \author Kelton Halbert - NWS Storm Prediction Center/OU-CIWRO
+ *
+ * \brief Compue the wind shear for a given sharp::PressureLayer 
+ *
+ * Computes the U and V components of the wind shear over a 
+ * sharp::PressureLayer given the vertical sounding arrays 
+ * of pressure, u_wind, v_wind, and their length. 
+ *
+ * \param layer     sharp::PressureLayer      
+ * \param pres      (hPa)
+ * \param u_wind    (kts or m/s)
+ * \param v_wind    (kts or m/s)
+ * \param num_levs  (length of arrays)
+ * \return sharp::WindComponents    {shear_u, shear_v}
+ */
+WindComponents wind_shear(const PressureLayer& layer,
+                          const float* pres,   const float* u_wind,
+                          const float* v_wind, int num_levs);
+
 
 }
 
