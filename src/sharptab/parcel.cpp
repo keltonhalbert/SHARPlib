60e4ad02374fa254cba087ffe6c0ae43c5c9219d
Kelton Halbert <kelton.halbert@noaa.gov>
2022-11-09 17:13:31 +0000

    Some restructuring of Profiles and Parcels amidst trying to flesh out parcel lifting logic. Added test file and CMakeLists entries for testing parcel routines.

--- src/sharptab/parcel.cpp
+++ src/sharptab/parcel.cpp
@@ -12,8 +12,28 @@
  * Based on NSHARP routines originally written by
  * John Hart and Rich Thompson at SPC. 
  */
+#include "constants.h"
+#include "interp.h"
+#include "parcel.h"
+#include "utils.h"
 
 namespace sharp {
+
+
+Parcel::Parcel() {
+    pcl_pres = MISSING;
+    pcl_tmpc = MISSING;
+    pcl_dwpc = MISSING;
+    
+    lcl_pressure = MISSING;
+    lfc_pressure = MISSING;
+    eql_pressure = MISSING;
+    mpl_pressure = MISSING;
+
+    cape = 0.0;
+    cinh = 0.0;
+}
+
 
 // Trying to mentally sketch out how I want parcel lifting
 // and CAPE/CINH integration to work. I want it to be modular
@@ -29,6 +49,40 @@ namespace sharp {
 // 3) Lift the parcel/compute temperature trace
 // 4) Set LCL/LFC/EL values
 // 5) Integrate CAPE/CINH
+
+void define_parcel(Profile* prof, Parcel* pcl, LPL source) {
+    pcl->source = source;
+
+    switch(source) {
+        case LPL::SFC:
+            // set the parcel attributes to the surface
+            pcl->pcl_pres = prof->pres[0];
+            pcl->pcl_tmpc = prof->tmpc[0];
+            pcl->pcl_dwpc = prof->dwpc[0]; 
+            break;
+        case LPL::FCST:
+            // call forecast parcel routine
+            break;
+        case LPL::MU:
+            // call the most unstable parcel routine
+            break;
+        case LPL::ML:
+            // call the mixed layer parcel routine
+            break;
+        case LPL::USR:
+            // Parcel attributes are user defined, do
+            // nothing then?
+            break;
+        case LPL::EIL: 
+            // Call the mean effective inflow layer parcel
+            break;
+    };
+}
+
+
+
+
+
 
 } // end namespace sharp
 
---
+++ tests/test_parcel.cpp
@@ -0,0 +1,12 @@
+#define DOCTEST_CONFIG_IMPLEMENT_WITH_MAIN
+
+#include "doctest.h"
+#include "constants.h"
+#include "profile.h"
+#include "parcel.h"
+
+TEST_CASE("Testing parcel definitions") {
+
+    sharp::Profile prof(10, sharp::Source::Observed);
+
+}
