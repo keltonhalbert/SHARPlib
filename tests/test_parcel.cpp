c228197f1a9a5cd8b8bcd13a1e7f3f04230bc3b4
Kelton Halbert <kelton.halbert@noaa.gov>
2022-11-11 18:45:12 +0000

    fixed test

--- tests/test_parcel.cpp
+++ tests/test_parcel.cpp
@@ -8,6 +8,17 @@
 TEST_CASE("Testing parcel definitions") {
 
     sharp::Profile prof(10, sharp::Source::Observed);
+
+    // need to fill with dummy data to prevent
+    // segmentation fault
+    for (int i = 0; i < 10; i++) {
+        prof.pres[i] = 1000.0 - 100.0*i;
+        prof.hght[i] = 0 + 1000.0*i;
+        prof.tmpc[i] = i;
+        prof.dwpc[i] = i;
+
+    }
+
     sharp::Parcel pcl;
 
     sharp::lifter_wobus lifter;
