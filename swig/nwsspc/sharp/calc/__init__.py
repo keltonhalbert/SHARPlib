f5041933c8123ef754d89d6d3b62d0e1c293c2b5
Kelton Halbert <kelton.halbert@noaa.gov>
2022-11-30 19:35:25 +0000

    added module __init__.py script that loads each submodule

---
+++ swig/nwsspc/sharp/calc/__init__.py
@@ -0,0 +1,7 @@
+import nwsspc.sharp.calc.interp as interp
+import nwsspc.sharp.calc.params as params
+import nwsspc.sharp.calc.parcel as parcel
+import nwsspc.sharp.calc.profile as profile
+import nwsspc.sharp.calc.thermo as thermo
+import nwsspc.sharp.calc.utils as utils
+import nwsspc.sharp.calc.winds as winds
