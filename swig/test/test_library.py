9f7d1cdc46dce918afdcc1ee921f365a97b02102
Kelton Halbert <kelton.halbert@noaa.gov>
2022-12-01 18:06:30 +0000

    added testing on hires sounding file, and it works!

--- swig/test/test_library.py
+++ swig/test/test_library.py
@@ -1,11 +1,19 @@
+import pandas as pd
 import numpy as np
-import nwsspc.sharp.calc.winds as winds
+
+## fun little issue is that numpy and pandas
+## needs to be imported first, or bad things
+## happen
+import nwsspc.sharp.calc.constants as constants
+import nwsspc.sharp.calc.profile as profile
 import nwsspc.sharp.calc.interp as interp
+import nwsspc.sharp.calc.thermo as thermo
+import nwsspc.sharp.calc.parcel as parcel
+import nwsspc.sharp.calc.winds as winds
 import nwsspc.sharp.calc.utils as utils
 
 
 def test_interp():
-
     pres = np.arange(50.0, 1010.0, 50.0, dtype='float32')[::-1]
     hght = np.linspace(0, 15000, pres.shape[0]).astype('float32')
     data = np.linspace(1, 100, pres.shape[0]).astype('float32')
@@ -59,12 +67,41 @@ def test_winds():
     helicity = winds.helicity(hght_layer, strm_motnv, hght, uwin, vwin)
     print(helicity)
 
+def test_thermo():
+    pres = np.arange(50.0, 1010.0, 50.0, dtype='float32')[::-1]
+    hght = np.linspace(0, 15000, pres.shape[0]).astype('float32')
+    tmpc = np.repeat(10.0, pres.shape[0]).astype('float32')
+
+    layer = utils.HeightLayer(0, 3000.0)
+    lapse_rate = thermo.lapse_rate(layer, hght, tmpc)
+    print(lapse_rate)
+
+def test_parcel():
+    names = ["pres", "hght", "tmpc", "dwpc", "wdir", "wspd"]
+    snd_df = pd.read_csv("./hires-SPC.txt", delimiter=",", comment="%", names=names, skiprows=5)
+
+    pres = snd_df["pres"].to_numpy().astype('float32')
+    hght = snd_df["hght"].to_numpy().astype('float32')
+    tmpc = snd_df["tmpc"].to_numpy().astype('float32')
+    dwpc = snd_df["dwpc"].to_numpy().astype('float32')
+    wdir = snd_df["wdir"].to_numpy().astype('float32')
+    wspd = snd_df["wspd"].to_numpy().astype('float32')
+
+    prof = profile.create_profile(pres, hght, tmpc, dwpc, wspd, wdir, 
+                                  profile.Source_Observed, False) 
+    pcl = parcel.Parcel()
+
+    parcel.define_parcel(prof, pcl, parcel.LPL_MU)
+    parcel.parcel_wobf(prof, pcl)
+    print(pcl.cape, pcl.cinh)
+
 
 def main():
-
     test_interp()
     test_utils()
     test_winds()
+    test_thermo()
+    test_parcel()
 
 if __name__ == "__main__":
     main()
