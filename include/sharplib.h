2f7b3df526e000b041364e3da6e79bd7c1f4eb46
Kelton Halbert <kelton.halbert@noaa.gov>
2022-12-01 21:10:08 +0000

    added wrapper header to library

---
+++ include/sharplib.h
@@ -0,0 +1,29 @@
+/**
+ * \file
+ * \brief Wrapper header for the entire SHARP library
+ *
+ * \author  
+ *   Kelton Halbert                  \n
+ *   Email: kelton.halbert@noaa.gov  \n
+ *   License: Apache 2.0             \n
+ * \date   2022-12-01
+ *
+ * Written for the NWS Storm Predidiction Center \n
+ * Based on NSHARP routines originally written by
+ * John Hart and Rich Thompson at SPC. 
+ */
+
+#ifndef __SHARPLIB
+#define __SHARPLIB
+
+#include "constants.h"
+#include "interp.h"
+#include "utils.h"
+#include "thermo.h"
+#include "winds.h"
+#include "profile.h"
+#include "parcel.h"
+#include "params.h"
+
+
+#endif
