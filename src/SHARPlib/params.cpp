325033256744d022edd6ac4d65fdf50b0d0ebe30
Kelton Halbert <kelton.halbert@noaa.gov>
2022-12-07 18:40:35 +0000

    began filling in some params and updated python build instructions slightly

--- src/SHARPlib/params.cpp
+++ src/SHARPlib/params.cpp
@@ -12,9 +12,45 @@
  * John Hart and Rich Thompson at SPC. 
  */
 
+
+#include <SHARPlib/constants.h>
 #include <SHARPlib/utils.h>
 
 namespace sharp {
+
+
+
+float energy_helicity_index(float cape, float helicity) {
+#ifndef NO_QC
+    if ((cape == MISSING) || (helicity == MISSING)) {
+        return MISSING;
+    }
+#endif
+    return (cape * helicity) / 160000.0;
+}
+
+
+float supercell_composite_parameter(float mu_cape, float eff_srh,
+                                                   float eff_shear) {
+#ifndef NO_QC
+    if ((mu_cape == MISSING) || (eff_srh == MISSING) || (eff_shear == MISSING)) {
+        return MISSING;
+    }
+#endif
+
+    if (eff_shear > 20.0) {
+        eff_shear = 20.0;
+    }
+    else if (eff_shear < 10.0) {
+        eff_shear = 0.0;
+    }
+
+    float mu_cape_term = mu_cape / 1000.0;
+    float eff_srh_term = eff_srh / 50.0;
+    float eff_shear_term = eff_shear / 20.0;
+
+    return mu_cape_term * eff_srh_term * eff_shear_term;
+}
 
 
 } // end namespace sharp
