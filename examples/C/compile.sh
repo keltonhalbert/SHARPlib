151c2c6065e0f86338d1604d1885467781160647
Kelton Halbert <kelton.halbert@noaa.gov>
2023-05-18 19:24:09 +0000

    added optimization flags to build script and also added interpolation wrapper to main program

--- examples/C/compile.sh
+++ examples/C/compile.sh
@@ -1,3 +1,3 @@
-g++ --std=c++17 -DCWRAP -I../../include/ -I../../external/fmt/include/ -c ../../src/SHARPlib/*.cpp ../../src/SHARPlib/CWrap/*.cpp
+g++ --std=c++17 -O3 -I../../include/ -I../../external/fmt/include/ -c ../../src/SHARPlib/*.cpp ../../src/SHARPlib/CWrap/*.cpp
 ar rcs SHARPlib_C.a *.o
-gcc -I../../include -o main main.c SHARPlib_C.a -lstdc++ -lm
+gcc -O3 -I../../include -o main main.c SHARPlib_C.a -lstdc++ -lm
