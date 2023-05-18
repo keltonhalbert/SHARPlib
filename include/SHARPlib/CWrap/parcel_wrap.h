6d71945a465b8341b0d3e9a54c6c9a123b307d8e
Kelton Halbert <kelton.halbert@noaa.gov>
2023-05-18 18:36:22 +0000

    Finished implementing wrappers to the parcel API

--- include/SHARPlib/CWrap/parcel_wrap.h
+++ include/SHARPlib/CWrap/parcel_wrap.h
@@ -5,6 +5,8 @@
 extern "C" {
 #endif
 
+#include <SHARPlib/CWrap/profile_wrap.h>
+
 struct sharp_Parcel {
     void *obj;
 };
@@ -23,6 +25,21 @@ float sharp_Parcel_get_el_pres(sharp_Parcel_t* pcl);
 float sharp_Parcel_get_cape(sharp_Parcel_t* pcl);
 float sharp_Parcel_get_cinh(sharp_Parcel_t* pcl);
 int sharp_Parcel_get_lpl(sharp_Parcel_t* pcl);
+
+void sharp_define_parcel(sharp_Profile_t* prof, sharp_Parcel_t* pcl,
+                         int source);
+
+void sharp_define_custom_parcel(sharp_Parcel_t* pcl, float pres, float tmpc,
+                                float dwpc);
+
+void sharp_lift_parcel_wobf(sharp_Profile_t* prof, sharp_Parcel_t* pcl);
+
+void sharp_find_lfc_el(sharp_Parcel_t* pcl, const float* pres,
+                       const float* hght, const float* buoy, int NZ);
+
+void sharp_cape_cinh(sharp_Profile_t* prof, sharp_Parcel_t* pcl);
+
+void sharp_parcel_wobf(sharp_Profile_t* prof, sharp_Parcel_t* pcl);
 
 #ifdef __cplusplus
 }
--- src/SHARPlib/CWrap/parcel_wrap.cpp
+++ src/SHARPlib/CWrap/parcel_wrap.cpp
@@ -64,3 +64,50 @@ int sharp_Parcel_get_lpl(sharp_Parcel_t* p) {
 	return static_cast<int>(static_cast<sharp::Parcel*>(p->obj)->source);
 }
 
+void sharp_define_parcel(sharp_Profile_t* prof, sharp_Parcel_t* pcl,
+                         int source) {
+    if ((prof == NULL) || (pcl == NULL)) return;
+    sharp::Profile* pf = static_cast<sharp::Profile*>(prof->obj);
+    sharp::Parcel* pc = static_cast<sharp::Parcel*>(pcl->obj);
+    sharp::LPL src = static_cast<sharp::LPL>(source);
+    sharp::define_parcel(pf, pc, src); 
+}
+
+void sharp_define_custom_parcel(sharp_Parcel_t* pcl, float pres, float tmpc,
+                                float dwpc) {
+    if (pcl == NULL) return;
+    sharp::Parcel* pc = static_cast<sharp::Parcel*>(pcl->obj);
+    pc->pres = pres;
+    pc->tmpc = tmpc;
+    pc->dwpc = dwpc;
+}
+
+void sharp_lift_parcel_wobf(sharp_Profile_t* prof, sharp_Parcel_t* pcl) {
+    if ((prof == NULL) || (pcl == NULL)) return;
+    sharp::Profile* pf = static_cast<sharp::Profile*>(prof->obj);
+    sharp::Parcel* pc = static_cast<sharp::Parcel*>(pcl->obj);
+    constexpr sharp::lifter_wobus lifter;
+    sharp::lift_parcel(lifter, pf, pc);
+}
+
+void sharp_find_lfc_el(sharp_Parcel_t* pcl, const float* pres,
+                       const float* hght, const float* buoy, int NZ) {
+    if (pcl == NULL) return;
+    sharp::Parcel* pc = static_cast<sharp::Parcel*>(pcl->obj);
+    sharp::find_lfc_el(pc, pres, hght, buoy, NZ);
+}
+
+void sharp_cape_cinh(sharp_Profile_t* prof, sharp_Parcel_t* pcl) {
+    if ((prof == NULL) || (pcl == NULL)) return;
+    sharp::Profile* pf = static_cast<sharp::Profile*>(prof->obj);
+    sharp::Parcel* pc = static_cast<sharp::Parcel*>(pcl->obj);
+    sharp::cape_cinh(pf, pc);
+}
+
+void sharp_parcel_wobf(sharp_Profile_t* prof, sharp_Parcel_t* pcl) {
+    if ((prof == NULL) || (pcl == NULL)) return;
+    sharp::Profile* pf = static_cast<sharp::Profile*>(prof->obj);
+    sharp::Parcel* pc = static_cast<sharp::Parcel*>(pcl->obj);
+    sharp::parcel_wobf(pf, pc);
+}
+
