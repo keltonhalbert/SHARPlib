de263bfd479ceec5c5591dc35c12a57a95c9ea1e
Kelton Halbert <kelton.halbert@noaa.gov>
2023-05-24 19:46:19 +0000

    improved the logic of the interpolation routines, and removed redundant and unecessary checks. Next step will be to template the very common elements between the two.

--- src/SHARPlib/interp.cpp
+++ src/SHARPlib/interp.cpp
@@ -32,18 +32,8 @@ float interp_height(float height_val, const float* height_arr,
 #endif
 
     constexpr auto comp = std::less<float>();
-    int idx_bot = lower_bound(height_arr, num_levs, height_val, comp);
-	int idx_top = upper_bound(height_arr, num_levs, height_val, comp);
-	// only decrements if true
-	idx_bot -= ((height_arr[idx_bot] > height_val) && (idx_bot > 0)); 
-	//int idx_top = idx_bot + 1;
-
-    if ((idx_bot == idx_top) && (height_arr[idx_bot] == height_val)) {
-        return data_arr[idx_bot];
-    }
-    else if (idx_bot == idx_top) {
-        return MISSING;
-    }
+    int idx_top = upper_bound(height_arr, num_levs, height_val, comp);
+    int idx_bot = idx_top - 1;
 
 #ifndef NO_QC
     for (; idx_bot > 0; --idx_bot) {
@@ -53,18 +43,17 @@ float interp_height(float height_val, const float* height_arr,
     for (; idx_top < num_levs; ++idx_top) {
         if (data_arr[idx_top] != MISSING) break;
     }
+
+    // in the case the data are still missing at this point,
+    // return a missing value
+    if ((data_arr[idx_bot] == MISSING) || (data_arr[idx_top] == MISSING))
+        return MISSING;
 #endif
 
     float height_bot = height_arr[idx_bot];
     float height_top = height_arr[idx_top];
     float data_bot = data_arr[idx_bot];
     float data_top = data_arr[idx_top];
-
-#ifndef NO_QC
-    // if we didn't manage to find data, or our
-    // profile data is missing, return missing
-    if ((data_bot == MISSING) || (data_top == MISSING)) return MISSING;
-#endif
 
     // normalize the distance between values
     // to a range of 0-1 for the lerp routine
@@ -86,19 +75,9 @@ float interp_pressure(float pressure_val, const float* pressure_arr,
     }
 #endif
 
-	constexpr auto comp = std::greater<float>();
-	int idx_bot = lower_bound(pressure_arr, num_levs, pressure_val, comp);
-	int idx_top = upper_bound(pressure_arr, num_levs, pressure_val, comp);
-	// only decrements if true
-	idx_bot -= ((pressure_arr[idx_bot] < pressure_val) && (idx_bot > 0)); 
-
-    if ((idx_bot == idx_top) && (pressure_arr[idx_bot] == pressure_val)) {
-        return data_arr[idx_bot];
-    }
-
-    else if (idx_bot == idx_top) {
-        return MISSING;
-    }
+    constexpr auto comp = std::greater<float>();
+    int idx_top = upper_bound(pressure_arr, num_levs, pressure_val, comp);
+    int idx_bot = idx_top - 1;
 
 #ifndef NO_QC
     for (; idx_bot > 0; --idx_bot) {
@@ -108,18 +87,17 @@ float interp_pressure(float pressure_val, const float* pressure_arr,
     for (; idx_top < num_levs; ++idx_top) {
         if (data_arr[idx_top] != MISSING) break;
     }
+
+    // in the case the data are still missing at this point,
+    // return a missing value
+    if ((data_arr[idx_bot] == MISSING) || (data_arr[idx_top] == MISSING))
+        return MISSING;
 #endif
 
     float pressure_bot = pressure_arr[idx_bot];
     float pressure_top = pressure_arr[idx_top];
     float data_bot = data_arr[idx_bot];
     float data_top = data_arr[idx_top];
-
-#ifndef NO_QC
-    // if we didn't manage to find data, or our
-    // profile data is missing, return missing
-    if ((data_bot == MISSING) || (data_top == MISSING)) return MISSING;
-#endif
 
     // In order to linearly interpolate pressure properly, distance needs
     // to be calculated in log10(pressure) coordinates and normalized
