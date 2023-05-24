8d699156dfd32fdac5ea5093606d7c42572394f1
Kelton Halbert <kelton.halbert@noaa.gov>
2023-05-24 19:46:34 +0000

    added some new tests to make sure things are robust

--- tests/test_layer.cpp
+++ tests/test_layer.cpp
@@ -29,17 +29,18 @@ TEST_CASE("Testing layer_max over pressure layer") {
     sharp::PressureLayer layer2(1100.0, 50.0);  // out of bounds recovery
     sharp::PressureLayer layer3(1000.0, 850.0); // max is not in layer
     sharp::PressureLayer layer4(800.0, 650.0); // max is just above layer
+    sharp::PressureLayer layer5(550.0, 400.0); // max is just below layer
     float pmax = -9999.0;
 
     CHECK(sharp::layer_max(layer1, pres, data, 10, &pmax) == 10.0);
     CHECK(pmax == 600.0);
-
     CHECK(sharp::layer_max(layer2, pres, data, 10, nullptr) == 10.0);
     CHECK(sharp::layer_max(layer3, pres, data, 10, nullptr) ==  0.0);
-
     CHECK(sharp::layer_max(layer4, pres, data, 10, &pmax) ==  
                                      doctest::Approx(4.80749));
     CHECK(pmax == 650.0);
+    CHECK(sharp::layer_max(layer5, pres, data, 10, &pmax) ==  
+                                     doctest::Approx(5.22758));
 }
 
 
@@ -48,17 +49,19 @@ TEST_CASE("Testing layer_min over pressure layer") {
     float pres[10] = {1000.0, 900.0, 800.0, 700.0, 600.0, 
                       500.0, 400.0, 300.0, 200.0, 100.0};
     float data[10] = {0, 0, 0, 0, -10, 0, 0, 0, 0, 0};
-    sharp::PressureLayer layer1(1000.0, 600.0); // max at top of layer
+    sharp::PressureLayer layer1(1000.0, 600.0); // min at top of layer
     sharp::PressureLayer layer2(1100.0, 50.0);  // out of bounds recovery
-    sharp::PressureLayer layer3(1000.0, 850.0); // max is not in layer
-    sharp::PressureLayer layer4(800.0, 650.0); // max is just above layer
+    sharp::PressureLayer layer3(1000.0, 850.0); // min is not in layer
+    sharp::PressureLayer layer4(800.0, 650.0); // min is just above layer
+    sharp::PressureLayer layer5(550.0, 400.0); // min is just below layer
 
     CHECK(sharp::layer_min(layer1, pres, data, 10) == -10.0);
     CHECK(sharp::layer_min(layer2, pres, data, 10) == -10.0);
     CHECK(sharp::layer_min(layer3, pres, data, 10) ==   0.0);
-
     CHECK(sharp::layer_min(layer4, pres, data, 10) ==  
                                     doctest::Approx(-4.80749));
+    CHECK(sharp::layer_min(layer5, pres, data, 10) ==  
+                                    doctest::Approx(-5.22758));
 }
 
 
@@ -86,11 +89,11 @@ TEST_CASE("Testing layer_min over height layer") {
     float hght[10] = {1000.0, 2000.0, 3000.0, 4000.0, 5000.0, 
                       6000.0, 7000.0, 8000.0, 9000.0, 10000.0};
     float data[10] = {0, 0, 0, 0, -10, 0, 0, 0, 0, 0};
-    sharp::HeightLayer layer1(1000.0, 6000.0); // max at top of layer
+    sharp::HeightLayer layer1(1000.0, 6000.0); // min at top of layer
     sharp::HeightLayer layer2(-100.0, 20000.0);  // out of bounds recovery
-    sharp::HeightLayer layer3(6000.0, 10000.0); // max is not in layer
-    sharp::HeightLayer layer4(1000.0, 4750.0); // max is just above layer
-    sharp::HeightLayer layer5(5250.0, 9000.0); // max is just below layer
+    sharp::HeightLayer layer3(6000.0, 10000.0); // min is not in layer
+    sharp::HeightLayer layer4(1000.0, 4750.0); // min is just above layer
+    sharp::HeightLayer layer5(5250.0, 9000.0); // min is just below layer
 
     CHECK(sharp::layer_min(layer1, hght, data, 10) == -10.0);
     CHECK(sharp::layer_min(layer2, hght, data, 10) == -10.0);
