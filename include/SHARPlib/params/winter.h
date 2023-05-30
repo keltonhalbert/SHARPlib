d5f75a91734517eee9c0e6340ed75742057d910c
Kelton Halbert <kelton.halbert@noaa.gov>
2023-05-30 17:59:59 -0500

    added empty placeholder files for other params

---
+++ include/SHARPlib/params/winter.h
@@ -0,0 +1,24 @@
+/**
+ * \file
+ * \brief Winter weather parameters
+ * \author
+ *   Kelton Halbert                  \n
+ *   Email: kelton.halbert@noaa.gov  \n
+ *   License: Apache 2.0             \n
+ * \date   2023-05-30
+ *
+ * Written for the NWS Storm Predidiction Center \n
+ * Based on NSHARP routines originally written by
+ * John Hart and Rich Thompson at SPC.
+ */
+
+#ifndef __SHARP_PARAMS_WINTER_H__
+#define __SHARP_PARAMS_WINTER_H__
+
+namespace sharp {
+
+
+
+} // namespace sharp
+
+#endif // __SHARP_PARAMS_WINTER_H__
