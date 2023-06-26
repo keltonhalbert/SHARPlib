098c7457de88a7802951a694210bc4f565d49ffe
Kelton Halbert <kelton.halbert@noaa.gov>
2023-06-26 15:21:08 -0500

    completely rewrote the parcel testing to use an analytic sounding rather than a known file or dataset. This allows for controlling of data resolution as well as atmospheric attributes. likely generalize these lambdas into their own little header file for the testing portion of the library

--- tests/unit/test_parcel.cpp
+++ tests/unit/test_parcel.cpp
@@ -1,215 +1,249 @@
-#define DOCTEST_CONFIG_IMPLEMENT_WITH_MAIN
+#define doctest_config_implement_with_main
+
+#include <sharplib/constants.h>
+#include <sharplib/layer.h>
+#include <sharplib/parcel.h>
+#include <sharplib/profile.h>
+#include <sharplib/winds.h>
 
 #include <iostream>
-#include <fstream>
-#include <vector>
+#include <memory>
 #include <string>
-#include <chrono>
 
 #include "doctest.h"
-#include <SHARPlib/constants.h>
-#include <SHARPlib/profile.h>
-#include <SHARPlib/winds.h>
-#include <SHARPlib/parcel.h>
 
-// splits a string on a delimiter and stores in a vector
-std::vector<std::string> split(std::string& s, std::string delimiter) {
-    size_t pos_start = 0;
-    size_t pos_end = 0;
-    size_t delim_len = delimiter.length();
-    std::string token;
-    std::vector<std::string> res;
+auto array_from_range = [](const float bottom, const float top,
+                           const std::ptrdiff_t size) {
+    auto arr = std::make_unique<float[]>(size);
+    float delta = (top - bottom) / static_cast<float>(size);
+    for (int k = 0; k < size; ++k) {
+        arr[k] = bottom + static_cast<float>(k) * delta;
+    }
+    return arr;
+};
 
-    while ((pos_end = s.find(delimiter, pos_start)) != std::string::npos) {
-        token = s.substr (pos_start, pos_end - pos_start);
-        pos_start = pos_end + delim_len;
-        res.push_back (token);
+auto tmpk_snd = [](const float tmpk_sfc, const float delta_tmpk_cap,
+                   const float delta_tmpk_troposphere, const float hght_pbl_top,
+                   const float hght_trop_top, const float height[],
+                   const std::ptrdiff_t size) {
+    // get the indices corresponding to the pbl top
+    // and the tropopause...
+    sharp::heightlayer free_troposphere =
+        sharp::heightlayer(hght_pbl_top, hght_trop_top);
+    sharp::layerindex idx =
+        sharp::get_layer_index(free_troposphere, height, size);
+
+    const float dse_pbl = sharp::moist_static_energy(0.0f, tmpk_sfc, 0.0f);
+    // use the temperature lapse rate to get a
+    // dry static energy rate of change
+    const float delta_dse_trop =
+        sharp::gravity - (sharp::cp_dryair * delta_tmpk_troposphere);
+
+    auto tmpk_arr = std::make_unique<float[]>(size);
+    tmpk_arr[0] = tmpk_sfc;
+
+    // calculate the temperature of the boundary layer
+    // profile assuming a constant dry static energy
+    for (int k = 1; k < idx.kbot; ++k) {
+        tmpk_arr[k] = (dse_pbl - height[k] * sharp::gravity) / sharp::cp_dryair;
     }
 
-    res.push_back (s.substr (pos_start));
-    return res;
-}
+    // add a capping inversion to the top of the boundary layer
+    // and use that as the starting dry static energy for the
+    // free troposphere, which will then be modified by the
+    // tropospheric lapse rate.
+    const float dse_troposphere_bottom = sharp::moist_static_energy(
+        height[idx.kbot], tmpk_arr[idx.kbot - 1] + delta_tmpk_cap, 0.0f);
 
-void build_profile(sharp::Profile* prof, std::vector<std::string>& row, int idx) {
-    float pres = std::stof(row[0]) * 100.0;
-    float hght = std::stof(row[1]);
-    float tmpk = std::stof(row[2]) + sharp::ZEROCNK;
-    float dwpk = std::stof(row[3]) + sharp::ZEROCNK;
-    float wdir = std::stof(row[4]);
-    float wspd = std::stof(row[5]);
-
-    prof->pres[idx] = pres; 
-    prof->hght[idx] = hght;
-    prof->tmpk[idx] = tmpk;
-    prof->dwpk[idx] = dwpk;
-    prof->wdir[idx] = wdir;
-    prof->wspd[idx] = wspd;
-
-    prof->mixr[idx] = sharp::mixratio(pres, dwpk);
-    prof->vtmp[idx] = sharp::virtual_temperature(tmpk, prof->mixr[idx]);
-    prof->theta[idx] = sharp::theta(pres, tmpk, sharp::THETA_REF_PRESSURE);
-    prof->theta_e[idx] = sharp::thetae(pres, tmpk, dwpk);
-    
-    sharp::WindComponents uv = sharp::vector_to_components(wspd, wdir);
-
-    prof->uwin[idx] = uv.u;
-    prof->vwin[idx] = uv.v;
-}
-
-sharp::Profile* read_sounding(std::string filename) {
-    std::ifstream sndfile(filename);
-    std::string line;
-
-    std::string begin = "%RAW%";
-    std::string end = "%END%";
-    bool found_begin = false;
-    bool found_end = false;
-    int NLINES = 0;
-    // first iteration - count the number of data
-    // rows so that we can allocate arrays of appropraite
-    // size to store the data
-    if (sndfile.is_open()) {
-        while (std::getline(sndfile, line)) {
-            if (line == end) found_end = true;
-
-            // check if we are within the data section
-            if (found_begin && !found_end) {
-                NLINES += 1;
-            }
-            // see if the data section begind
-            if (line == begin) found_begin = true;
-        }
+    // calculate the temperature profile of the free troposphere
+    for (int k = idx.kbot; k < idx.ktop + 1; ++k) {
+        float dse = dse_troposphere_bottom +
+                    delta_dse_trop * (height[k] - hght_pbl_top);
+        tmpk_arr[k] = (dse - sharp::gravity * height[k]) / sharp::cp_dryair;
     }
 
-    sharp::Profile* prof = new sharp::Profile(NLINES, sharp::Source::PFC);
-
-    // return to the beginning of the file
-    sndfile.clear();
-    sndfile.seekg(0);
-
-    found_begin = false;
-    found_end = false;
-    int idx = 0;
-    // now loop again, this time 
-    // splitting the data
-    if (sndfile.is_open()) {
-        // iterate over the lines in the file
-        while (std::getline(sndfile, line)) {
-            // loop-logic: if we're at the end of the
-            // data section, set that first. 
-            if (line == end) found_end = true;
-
-            // check if we are within the data section
-            if (found_begin && !found_end) {
-
-                // split the line on the comma
-                std::vector row = split(line, ",");
-
-                build_profile(prof, row, idx);
-
-                idx += 1;
-            }
-            // see if the data section begind
-            if (line == begin) found_begin = true;
-        }
-        sndfile.close();
-        std::cout << "Success reading: " << filename << std::endl;
-        std::cout << "Number of vertical levels: " << prof->NZ << std::endl;
-    
-        return prof;
+    // temperature above the tropopause is isothermal
+    for (int k = idx.ktop + 1; k < size; ++k) {
+        tmpk_arr[k] = tmpk_arr[idx.ktop];
     }
 
-    else {
-        std::cout << "Unable to open file: " << filename << std::endl;
-        return nullptr;
+    return tmpk_arr;
+};
+
+auto pres_dry_snd = [](const float pres_sfc, const float height[],
+                       const float tmpk[], const std::ptrdiff_t size) {
+    auto pres_arr = std::make_unique<float[]>(size);
+    pres_arr[0] = pres_sfc;
+
+    for (std::ptrdiff_t k = 1; k < size; ++k) {
+        float tmpk_mean = 0.5f * (tmpk[k] + tmpk[k - 1]);
+        float delta_z = height[k] - height[k - 1];
+        float delta_p =
+            -(sharp::gravity * delta_z) / (sharp::rdgas * tmpk_mean);
+        pres_arr[k] = pres_arr[k - 1] * std::exp(delta_p);
     }
-}
 
+    return pres_arr;
+};
 
-TEST_CASE("Testing new parcel definitions") {
-    std::string fname1 = "/users/khalbert/CODEBASE/NSHARP-server/unprocessed/sars_supercell/99050323f0.okc";
-    std::string fname2 = "/users/khalbert/Downloads/newSPC.txt";
-    std::string fname3 = "/users/khalbert/22112200.KEY";
-	std::string fname4 = "/home/kthalbert/CODEBASE/SHARP-calc/swig/test/hires-SPC.txt";
-	std::string fname5 = "/users/khalbert/CODEBASE/NSHARP-calc/swig/test/hires-SPC.txt";
+auto pres_moist_snd = [](const float pres_sfc, const float height[],
+                         const float tmpk[], const float mixr[],
+                         const std::ptrdiff_t size) {
+    auto pres_arr = std::make_unique<float[]>(size);
+    pres_arr[0] = pres_sfc;
 
-
-    auto start_time = std::chrono::system_clock::now();
-    sharp::Profile* prof = read_sounding(fname4);
-    auto end_time = std::chrono::system_clock::now();
-    auto duration = std::chrono::duration_cast<std::chrono::milliseconds>(end_time - start_time).count();
-
-    std::cout << "Reading file took: " << duration << "ms" << std::endl;
-    if (prof) {
-        sharp::Parcel sfc_pcl;
-        sharp::Parcel mu_pcl;
-        sharp::Parcel ml_pcl;
-
-        sharp::define_parcel(prof->pres, prof->tmpk, prof->dwpk, prof->mixr,
-                             prof->theta, prof->theta_e, prof->NZ, sfc_pcl,
-                             sharp::LPL::SFC);
-        sharp::define_parcel(prof->pres, prof->tmpk, prof->dwpk, prof->mixr,
-                             prof->theta, prof->theta_e, prof->NZ, mu_pcl,
-                             sharp::LPL::MU);
-        sharp::define_parcel(prof->pres, prof->tmpk, prof->dwpk, prof->mixr,
-                             prof->theta, prof->theta_e, prof->NZ, ml_pcl,
-                             sharp::LPL::ML);
-
-		static constexpr sharp::lifter_wobus lifter;
-        sharp::lifter_cm1 cm1;
-        cm1.ma_type = sharp::adiabat::pseudo_ice;
-        start_time = std::chrono::system_clock::now();
-
-		// Lift and integrate the surface parcel
-        sharp::lift_parcel(lifter, prof->pres, prof->vtmp, prof->buoyancy,
-                           prof->NZ, &sfc_pcl);
-        sharp::cape_cinh(prof->pres, prof->hght, prof->buoyancy, prof->NZ,
-                         &sfc_pcl);
-		// lift and integrate the ML parcel
-        sharp::lift_parcel(lifter, prof->pres, prof->vtmp, prof->buoyancy,
-                           prof->NZ, &ml_pcl);
-        sharp::cape_cinh(prof->pres, prof->hght, prof->buoyancy, prof->NZ,
-                         &ml_pcl);
-		// lift and integrate the MU parcel
-        sharp::lift_parcel(lifter, prof->pres, prof->vtmp, prof->buoyancy,
-                           prof->NZ, &mu_pcl);
-        sharp::cape_cinh(prof->pres, prof->hght, prof->buoyancy, prof->NZ,
-                         &mu_pcl);
-
-        end_time = std::chrono::system_clock::now();
-        duration = std::chrono::duration_cast<std::chrono::microseconds>(
-                       end_time - start_time).count();
-
-        std::cout << "Lifting 3 parcels took: " << duration << "us" << std::endl;
-        std::cout << "Average was: " << duration / 3.0 << "us" << std::endl;
-
-        std::cout << "SFC PCL\t";
-        std::cout << "LFC PRES: " << sfc_pcl.lfc_pressure << "\t"; 
-        std::cout << "EL PRES: " << sfc_pcl.eql_pressure << "\t"; 
-        std::cout << "CAPE: " << sfc_pcl.cape << "\t"; 
-        std::cout << "CINH: " << sfc_pcl.cinh << std::endl;
-
-        std::cout << "MU PCL\t";
-        std::cout << "LFC PRES: " << mu_pcl.lfc_pressure << "\t"; 
-        std::cout << "EL PRES: " << mu_pcl.eql_pressure << "\t"; 
-        std::cout << "CAPE: " << mu_pcl.cape << "\t"; 
-        std::cout << "CINH: " << mu_pcl.cinh << std::endl;
-
-        std::cout << "ML PCL\t";
-        std::cout << "LFC PRES: " << ml_pcl.lfc_pressure << "\t"; 
-        std::cout << "EL PRES: " << ml_pcl.eql_pressure << "\t"; 
-        std::cout << "CAPE: " << ml_pcl.cape << "\t"; 
-        std::cout << "CINH: " << ml_pcl.cinh << std::endl;
-
-        sharp::lift_parcel(cm1, prof->pres, prof->vtmp, prof->buoyancy,
-                           prof->NZ, &sfc_pcl);
-        sharp::cape_cinh(prof->pres, prof->hght, prof->buoyancy, prof->NZ, 
-                         &sfc_pcl);
-        std::cout << "CM1 SFC PCL\t";
-        std::cout << "CM1 LFC PRES: " << sfc_pcl.lfc_pressure << "\t"; 
-        std::cout << "CM1 EL PRES: " << sfc_pcl.eql_pressure << "\t"; 
-        std::cout << "CM1 CAPE: " << sfc_pcl.cape << "\t"; 
-        std::cout << "CM1 CINH: " << sfc_pcl.cinh << std::endl;
+    for (std::ptrdiff_t k = 1; k < size; ++k) {
+        const float tmpk_mean = 0.5f * (tmpk[k] + tmpk[k - 1]);
+        const float mixr_mean = 0.5f * (mixr[k] + mixr[k - 1]);
+        const float tv_mean = sharp::virtual_temperature(tmpk_mean, mixr_mean);
+        const float delta_z = height[k] - height[k - 1];
+        const float delta_p =
+            -(sharp::gravity * delta_z) / (sharp::rdgas * tv_mean);
+        pres_arr[k] = pres_arr[k - 1] * std::exp(delta_p);
     }
-    delete prof;
+
+    return pres_arr;
+};
+
+auto mixr_snd = [](const float pres_sfc, const float mixr_sfc,
+                   const float relh_troposphere, const float height[],
+                   const float tmpk[], const std::ptrdiff_t size) {
+    auto pres_arr = pres_dry_snd(pres_sfc, height, tmpk, size);
+    auto mixr_arr = std::make_unique<float[]>(size);
+    mixr_arr[0] = mixr_sfc;
+
+    for (std::ptrdiff_t k = 1; k < size; ++k) {
+        const float sat_mixr = sharp::mixratio(pres_arr[k], tmpk[k]);
+        mixr_arr[k] = relh_troposphere * sat_mixr;
+    }
+
+    return mixr_arr;
+};
+
+auto dwpk_snd = [](const float pres[], const float mixr[],
+                   const std::ptrdiff_t size) {
+    auto dwpk_arr = std::make_unique<float[]>(size);
+    for (std::ptrdiff_t k = 0; k < size; ++k) {
+        dwpk_arr[k] = sharp::temperature_at_mixratio(mixr[k], pres[k]);
+    }
+    return dwpk_arr;
+};
+
+auto vtmpk_snd = [](const float tmpk[], const float mixr[],
+                    const std::ptrdiff_t size) {
+    auto vtmpk_arr = std::make_unique<float[]>(size);
+    for (std::ptrdiff_t k = 0; k < size; ++k) {
+        vtmpk_arr[k] = sharp::virtual_temperature(tmpk[k], mixr[k]);
+    }
+    return vtmpk_arr;
+};
+
+auto theta_snd = [](const float pres[], const float tmpk[],
+                    const std::ptrdiff_t size) {
+    auto theta_arr = std::make_unique<float[]>(size);
+    for (std::ptrdiff_t k = 0; k < size; ++k) {
+        theta_arr[k] = sharp::theta(pres[k], tmpk[k]);
+    }
+    return theta_arr;
+};
+
+test_case("testing new parcel definitions") {
+    const std::ptrdiff_t n = 5000;
+    const float pres_sfc = 100000.0f;
+    const float tmpk_sfc = 301.5f;
+    const float mixr_sfc = 0.0157f;
+    const float relh_troposphere = 0.85f;
+    const float hght_sfc = 0.0f;
+    const float hght_top = 15000.0f;
+    const float hght_tropopause = 12000.0f;
+    const float delta_tmpk_cap = 1.0f;
+    const float delta_tmpk_trop = 0.00725f;
+    const float hght_pbl_top = 850.0f;
+
+    // initialize the analytical sounding using
+    // our input parameters
+    auto hght = array_from_range(hght_sfc, hght_top, n);
+    auto tmpk = tmpk_snd(tmpk_sfc, delta_tmpk_cap, delta_tmpk_trop,
+                         hght_pbl_top, hght_tropopause, hght.get(), n);
+    auto mixr = mixr_snd(pres_sfc, mixr_sfc, relh_troposphere, hght.get(),
+                         tmpk.get(), n);
+    auto pres = pres_moist_snd(pres_sfc, hght.get(), tmpk.get(), mixr.get(), n);
+    auto dwpk = dwpk_snd(pres.get(), mixr.get(), n);
+    auto vtmpk = vtmpk_snd(tmpk.get(), mixr.get(), n);
+
+    // allocate an array for our buoyancy data
+    auto buoy = std::make_unique<float[]>(n);
+
+    printf("%f\n", hght[1] - hght[0]);
+
+    sharp::parcel sfc_pcl;
+    sfc_pcl.pres = pres[0];
+    sfc_pcl.tmpk = tmpk[0];
+    sfc_pcl.dwpk = dwpk[0];
+
+    static constexpr sharp::lifter_wobus lifter;
+    sharp::lifter_cm1 cm1_pi;
+    sharp::lifter_cm1 cm1_pl;
+    sharp::lifter_cm1 cm1_ai;
+    sharp::lifter_cm1 cm1_al;
+    cm1_pi.ma_type = sharp::adiabat::pseudo_ice;
+    cm1_pl.ma_type = sharp::adiabat::pseudo_liq;
+    cm1_ai.ma_type = sharp::adiabat::adiab_ice;
+    cm1_al.ma_type = sharp::adiabat::adiab_liq;
+
+    // lift and integrate the surface parcel
+    sharp::lift_parcel(lifter, pres.get(), vtmpk.get(), buoy.get(), n,
+                       &sfc_pcl);
+    sharp::cape_cinh(pres.get(), hght.get(), buoy.get(), n, &sfc_pcl);
+
+    std::cout << "wobus lifter" << std::endl;
+    std::cout << "sfc pcl\t";
+    std::cout << "lfc pres: " << sfc_pcl.lfc_pressure << "\t";
+    std::cout << "el pres: " << sfc_pcl.eql_pressure << "\t";
+    std::cout << "cape: " << sfc_pcl.cape << "\t";
+    std::cout << "cinh: " << sfc_pcl.cinh << std::endl;
+
+    sharp::lift_parcel(cm1_pi, pres.get(), vtmpk.get(), buoy.get(), n,
+                       &sfc_pcl);
+    sharp::cape_cinh(pres.get(), hght.get(), buoy.get(), n, &sfc_pcl);
+
+    std::cout << "cm1 lifter (psuedo adiabatic with ice)" << std::endl;
+    std::cout << "sfc pcl\t";
+    std::cout << "lfc pres: " << sfc_pcl.lfc_pressure << "\t";
+    std::cout << "el pres: " << sfc_pcl.eql_pressure << "\t";
+    std::cout << "cape: " << sfc_pcl.cape << "\t";
+    std::cout << "cinh: " << sfc_pcl.cinh << std::endl;
+
+    sharp::lift_parcel(cm1_pl, pres.get(), vtmpk.get(), buoy.get(), n,
+                       &sfc_pcl);
+    sharp::cape_cinh(pres.get(), hght.get(), buoy.get(), n, &sfc_pcl);
+
+    std::cout << "cm1 lifter (psuedo adiabatic with no ice)" << std::endl;
+    std::cout << "sfc pcl\t";
+    std::cout << "lfc pres: " << sfc_pcl.lfc_pressure << "\t";
+    std::cout << "el pres: " << sfc_pcl.eql_pressure << "\t";
+    std::cout << "cape: " << sfc_pcl.cape << "\t";
+    std::cout << "cinh: " << sfc_pcl.cinh << std::endl;
+
+    sharp::lift_parcel(cm1_ai, pres.get(), vtmpk.get(), buoy.get(), n,
+                       &sfc_pcl);
+    sharp::cape_cinh(pres.get(), hght.get(), buoy.get(), n, &sfc_pcl);
+
+    std::cout << "cm1 lifter (adiabatic with ice)" << std::endl;
+    std::cout << "sfc pcl\t";
+    std::cout << "lfc pres: " << sfc_pcl.lfc_pressure << "\t";
+    std::cout << "el pres: " << sfc_pcl.eql_pressure << "\t";
+    std::cout << "cape: " << sfc_pcl.cape << "\t";
+    std::cout << "cinh: " << sfc_pcl.cinh << std::endl;
+
+    sharp::lift_parcel(cm1_al, pres.get(), vtmpk.get(), buoy.get(), n,
+                       &sfc_pcl);
+    sharp::cape_cinh(pres.get(), hght.get(), buoy.get(), n, &sfc_pcl);
+
+    std::cout << "cm1 lifter (adiabatic with no ice)" << std::endl;
+    std::cout << "sfc pcl\t";
+    std::cout << "lfc pres: " << sfc_pcl.lfc_pressure << "\t";
+    std::cout << "el pres: " << sfc_pcl.eql_pressure << "\t";
+    std::cout << "cape: " << sfc_pcl.cape << "\t";
+    std::cout << "cinh: " << sfc_pcl.cinh << std::endl;
 }
