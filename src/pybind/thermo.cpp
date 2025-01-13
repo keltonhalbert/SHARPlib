070dcb27c4707d4db2be390c5af1f49f353bd7d9
Kelton Halbert <kelton.halbert@noaa.gov>
2025-01-13 16:23:56 -0600

    The pybind11 bindings are now buildable using a pyproject.toml

--- src/pybind/thermo.cpp
+++ src/pybind/thermo.cpp
@@ -4,8 +4,13 @@
 // cland-format on
 #include <SHARPlib/thermo.h>
 
-
+namespace py = pybind11;
+// clang-format on
 PYBIND11_MODULE(thermo, m) {
-    m.doc() = "Sounding and Hodograph Analysis and Research Program Library (SHARPlib) :: Thermodynamic Routines";
-    m.def("lcl_temperature", &sharp::lcl_temperature);
+    m.doc() =
+        "Sounding and Hodograph Analysis and Research Program Library "
+        "(SHARPlib) :: Thermodynamic Routines";
+    m.def("lcl_temperature", &sharp::lcl_temperature,
+          "Compute the Lifted Condensation Level (LCL) temperature (K) using "
+          "the air temperature (K) and dewpoint temperature(K)");
 }
