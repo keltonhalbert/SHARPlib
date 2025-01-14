42e3a95672dc75dfeb2692a820402dec667baca3
Kelton Halbert <kelton.halbert@noaa.gov>
2025-01-13 20:45:25 -0600

    Tested out function vectorizing

--- src/pybind11/thermo.cpp
+++ src/pybind11/thermo.cpp
@@ -1,5 +1,6 @@
 // clang-format off
 #include <pybind11/pybind11.h>
+#include <pybind11/numpy.h>
 
 // cland-format on
 #include <SHARPlib/thermo.h>
@@ -11,6 +12,9 @@ PYBIND11_MODULE(thermo, m) {
         "Sounding and Hodograph Analysis and Research Program Library "
         "(SHARPlib) :: Thermodynamic Routines";
     m.def("lcl_temperature", &sharp::lcl_temperature,
+          "Compute the Lifted Condensation Level (LCL) temperature (K) using "
+          "the air temperature (K) and dewpoint temperature(K)");
+    m.def("lcl_temperature", py::vectorize(sharp::lcl_temperature),
           "Compute the Lifted Condensation Level (LCL) temperature (K) using "
           "the air temperature (K) and dewpoint temperature(K)");
 }
