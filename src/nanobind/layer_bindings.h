7845cb4c0373c1844bd2364744bb6ea145416c85
Kelton Halbert <kelton.halbert@noaa.gov>
2025-04-14 15:46:49 -0500

    continuing to flesh out the layer bindings

--- src/nanobind/layer_bindings.h
+++ src/nanobind/layer_bindings.h
@@ -55,7 +55,16 @@ void make_layer_bindings(nb::module_ m) {
         },
         nb::arg("layer"), nb::arg("pressure"),
         R"pbdoc(
-Finds the array indices corresponding to the given PressureLayer.
+Finds the interior array indices encapsulated by the given PressureLayer. 
+More specifically, the returned LayerIndex excludes the exact top and bottom 
+values (in mathematical notation, [bottom, top]). This behavior is due to the 
+fact many algorithms use interpolation to get the exact values of arbitrary
+top/bottom locations within a profile. 
+
+If layer.bottom or layer.top are out of bounds, this function 
+will truncate the layer to the coordinate range of data provided 
+by coord[] in an attempt to gracefully continue and produce a result. 
+This will modify the value of the given PressureLayer.
 
 Parameters:
     layer: PressureLayer
@@ -74,15 +83,93 @@ Returns:
         },
         nb::arg("layer"), nb::arg("height"),
         R"pbdoc(
-Finds the array indices corresponding to the given HeightLayer.
+Finds the interior array indices encapsulated by the given HeightLayer. 
+More specifically, the returned LayerIndex excludes the exact top and bottom 
+values (in mathematical notation, [bottom, top]). This behavior is due to the 
+fact many algorithms use interpolation to get the exact values of arbitrary
+top/bottom locations within a profile. 
+
+If layer.bottom or layer.top are out of bounds, this function 
+will truncate the layer to the coordinate range of data provided 
+by coord[] in an attempt to gracefully continue and produce a result. 
+This will modify the value of the given HeightLayer.
 
 Parameters:
     layer: HeightLayer
-    pressure: 1D NumPy array of heights
+    height: 1D NumPy array of heights
 
 Returns:
     A LayerIndex with {kbot, ktop}.
         )pbdoc");
+
+    m_layer.def(
+        "height_layer_to_pressure",
+        [](sharp::HeightLayer& layer,
+           const_prof_arr_t pressure,
+           const_prof_arr_t height,
+           const bool isAGL) -> sharp::PressureLayer {
+            return sharp::height_layer_to_pressure(
+                layer, 
+                pressure.data(), 
+                height.data(), 
+                height.size(), 
+                isAGL
+            );
+
+        },
+        nb::arg("layer"), nb::arg("pressure"), nb::arg("height"),
+        nb::arg("isAGL") = false,
+        R"pbdoc(
+Converts a HeightLayer to a PressureLayer via interpolation, with the
+optional argument to convert the HeightLayer from meters AGL to MSL by adding
+the station height to the HeightLayer. If for some strange reason 
+you provide a HeightLayer that is out of the bounds of height[], then
+the bottom and top of the output layer will be set to MISSING.
+
+Parameters:
+    layer: HeightLayer
+    pressure: 1D NumPy array of pressure
+    height: 1D NumPy array of heights
+    isAGL: Whether or not the station height needs to be added for interpolation (default: False) 
+
+    )pbdoc"
+    );
+
+    m_layer.def(
+        "pressure_layer_to_height",
+        [](
+            sharp::PressureLayer& layer,
+            const_prof_arr_t pressure,
+            const_prof_arr_t height,
+            const bool toAGL
+        ) -> sharp::HeightLayer {
+            return sharp::pressure_layer_to_height(
+                layer, 
+                pressure.data(), 
+                height.data(), 
+                height.size(),
+                toAGL
+            );
+
+        },
+        nb::arg("layer"), nb::arg("pressure"), nb::arg("height"),
+        nb::arg("toAGL") = false,
+        R"pbdoc(
+Converts a PressureLayer to a HeightLayer via interpolation, with the 
+optional argument to convert the HeightLayer to meters AGL by subtracting off 
+the station height from the returned HeightLayer. If for some strange reason
+you provide a PressureLayer that is out of the bounds of pressure[], then 
+the bottom and top of the output layer will be set to MISSING. 
+
+Parameters:
+    layer: PressureLayer
+    pressure: 1D NumPy array of pressure 
+    height: 1D NumPy array of heights
+    toAGL: Whether or not to subtract the station height from the HeightLayer (default: False)
+
+    )pbdoc"
+    );
+
 }
 
 #endif
