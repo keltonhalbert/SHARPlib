fe3f4d3b1b85361d575faf401a98900b92ae741d
Kelton Halbert <kelton.halbert@noaa.gov>
2025-04-14 15:46:34 -0500

    clang-format

--- src/nanobind/calc.cpp
+++ src/nanobind/calc.cpp
@@ -5,12 +5,13 @@
 #include "thermo_bindings.h"
 #include "interp_bindings.h"
 #include "layer_bindings.h"
-#include "parcel_bindings.h"
+//#include "parcel_bindings.h"
 
 // clang-format on
 NB_MODULE(calc, m) {
-    m.doc() = "Sounding and Hodograph Analysis and Research Program Library "
-              "(SHARPlib)";
+    m.doc() =
+        "Sounding and Hodograph Analysis and Research Program Library "
+        "(SHARPlib)";
 
     make_thermo_bindings(m);
     make_interp_bindings(m);
