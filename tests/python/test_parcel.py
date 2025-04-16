01187607da43b0131839d7aab02010943834996a
Kelton Halbert <kelton.halbert@noaa.gov>
2025-04-16 14:05:41 -0500

    seems like lifter_wobus is working

--- tests/python/test_parcel.py
+++ tests/python/test_parcel.py
@@ -7,4 +7,18 @@ from nwsspc.sharp.calc import constants
 
 
 def test_lifter_wobus():
-    pass
+    lifter = parcel.lifter_wobus()
+
+    pres = 100000.0
+    tmpk = 320.0
+    new_pres = 50000.0
+
+    assert (lifter(constants.MISSING, tmpk, new_pres) == constants.MISSING)
+    assert (lifter(pres, constants.MISSING, new_pres) == constants.MISSING)
+    assert (lifter(pres, tmpk, constants.MISSING) == constants.MISSING)
+
+    assert (np.isnan(lifter(np.nan, tmpk, new_pres)))
+    assert (np.isnan(lifter(pres, np.nan, new_pres)))
+    assert (np.isnan(lifter(pres, tmpk, np.nan)))
+
+    assert (lifter(pres, tmpk, new_pres) == pytest.approx(301.872528))
