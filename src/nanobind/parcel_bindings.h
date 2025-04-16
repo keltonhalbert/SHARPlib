949ec6365806f94d4a363910175d0a722e095f4c
Kelton Halbert <kelton.halbert@noaa.gov>
2025-04-16 14:43:46 -0500

    added lifter_cm1

--- src/nanobind/parcel_bindings.h
+++ src/nanobind/parcel_bindings.h
@@ -73,6 +73,66 @@ Parameters:
 Returns:
     The virtual temperature of the parcel (K)
              )pbdoc");
+
+    nb::class_<sharp::lifter_cm1>(m_parcel, "lifter_cm1", R"pbdoc(
+Use the CM1 moist lift calculations for adiabatic and pseudoadiabatic
+parcel ascent.
+            )pbdoc")
+        .def(nb::init<>())
+        .def_ro_static("lift_from_lcl", &sharp::lifter_cm1::lift_from_lcl,
+                       R"pbdoc(
+A static flag that helps the parcel lifting functions know where to lift from.
+The lifter_cm1 lifts from the last lifted level, rather than the LCL, because
+it is an iterative solver. This results in major performance improvements while 
+maintaining accuracy.
+                   )pbdoc")
+        .def_rw("ma_type", &sharp::lifter_cm1::ma_type, R"pbdoc(
+The type of moist adiabat to use, defined by sharp::adiabat
+            )pbdoc")
+        .def_rw("pressure_incr", &sharp::lifter_cm1::pressure_incr, R"pbdoc(
+The pressure increment (Pa) to use for the iterative solver. 
+             )pbdoc")
+        .def_rw("converge", &sharp::lifter_cm1::converge, R"pbdoc(
+The iterative convergence criteria (K)
+             )pbdoc")
+        .def("setup", &sharp::lifter_cm1::setup, nb::arg("lcl_pres"),
+             nb::arg("lcl_tmpk"), R"pbdoc(
+This function sets the total water mixing ratio for 
+adiabatic parcel ascent, and zeroes out the vapor,
+liquid, and ice mixing ratio from previous parcel 
+ascents. 
+
+Parameters:
+    lcl_pres: The LCL pressure (Pa)
+    lcl_tmpk: The LCL temperature (K)
+             )pbdoc")
+        .def("__call__", &sharp::lifter_cm1::operator(), nb::is_operator(),
+             nb::arg("pres"), nb::arg("tmpk"), nb::arg("new_pres"),
+             R"pbdoc(
+Lifts a parcel moist adiabatically/pseudoadiabatically using
+sharp::moist_adiabat_cm1.
+
+Parameters:
+    pres: Parcel pressure (Pa)
+    tmpk: Parcel temperature (K)
+    new_pres: Final level of parcel after lift (Pa)
+
+Returns:
+    The temperature of the lifted parcel (K)
+             )pbdoc")
+
+        .def("parcel_virtual_temperature",
+             &sharp::lifter_cm1::parcel_virtual_temperature, nb::arg("pres"),
+             nb::arg("tmpk"), R"pbdoc(
+Computes the virtual temperature of the parcel (after saturation).
+
+Parameters:
+    pres: Parcel pressure (Pa)
+    tmpk: Parcel temperature (K)
+
+Returns:
+    The virtual temperature of the parcel (K)
+             )pbdoc");
 }
 
 #endif
