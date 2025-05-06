c9779143dd8bc66a0987902eb082f0132a43bfe8
Kelton Halbert <kelton.halbert@noaa.gov>
2025-05-06 16:16:23 -0500

    I have opted to remoce the C wrappers until such a time as they are actually needed.

--- examples/C/compile.sh
+++
@@ -1,3 +0,0 @@
-g++ --std=c++17 -O3 -I../../include/ -I../../external/fmt/include/ -c ../../src/SHARPlib/*.cpp ../../src/SHARPlib/CWrap/*.cpp
-ar rcs SHARPlib_C.a *.o
-gcc -O3 -I../../include -o main main.c SHARPlib_C.a -lstdc++ -lm
