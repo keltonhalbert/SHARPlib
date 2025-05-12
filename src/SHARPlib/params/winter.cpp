0027df625228a18f2b2bdcc6222131f65f15dbe5
keltonhalbert <kthalbert@wisc.edu>
2025-05-12 14:14:31 -0500

    added dendritic growth zone to winter params

--- src/SHARPlib/params/winter.cpp
+++ src/SHARPlib/params/winter.cpp
@@ -11,6 +11,24 @@
  * John Hart and Rich Thompson at SPC.
  */
 
+#include <SHARPlib/constants.h>
+#include <SHARPlib/interp.h>
 #include <SHARPlib/params/winter.h>
 
-namespace sharp {}  // namespace sharp
+#include <cstddef>
+
+namespace sharp {
+
+PressureLayer dendritic_layer(const float pressure[], const float temperature[],
+                              const std::ptrdiff_t N) {
+    sharp::PressureLayer dgz = {sharp::MISSING, sharp::MISSING};
+
+    dgz.bottom =
+        find_first_pressure(-12.0f + ZEROCNK, pressure, temperature, N);
+    dgz.top = find_first_pressure(-17.0f, pressure, temperature, N);
+
+    // do_stuff
+    return dgz;
+};
+
+}  // namespace sharp
