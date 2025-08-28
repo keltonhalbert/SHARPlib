522716ad97db50a63f81a1fb257a7e6b78eedcae
Kelton Halbert <kelton.halbert@noaa.gov>
2025-08-28 15:43:01 -0500

    patch the submodules on import

--- src/nanobind/__init__.py
+++ src/nanobind/__init__.py
@@ -1,3 +1,4 @@
+import sys
 from . import _calc
 
 try:
@@ -8,4 +9,23 @@ except ImportError:
 __doc__ = _calc.__doc__
 
 __all__ = [s for s in dir(_calc) if not s.startswith('_')]
+
+def _patch_submodules():
+    """
+    A helper function to patch nanobind submodules for Sphinx.
+    """
+    for name in __all__:
+        if hasattr(_calc, name):
+            submodule = getattr(_calc, name)
+            
+            public_path = f"{__name__}.{name}"
+            
+            submodule.__name__ = public_path
+            
+            sys.modules[public_path] = submodule
+
+_patch_submodules()
+del _patch_submodules 
+del sys
+
 
