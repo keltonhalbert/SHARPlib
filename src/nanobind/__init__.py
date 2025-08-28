c9a36e9213fd27b7e17d8ce11749e806d274dbdd
Kelton Halbert <kelton.halbert@noaa.gov>
2025-08-28 15:24:34 -0500

    revised init

--- src/nanobind/__init__.py
+++ src/nanobind/__init__.py
@@ -1,26 +1,11 @@
-from ._calc import (
-    constants as constants,
-    interp as interp,
-    layer as layer,
-    params as params,
-    parcel as parcel,
-    thermo as thermo,
-    winds as winds,
-    __doc__ as __doc__,
-)
+from . import _calc
 
 try:
     from ._version import __version__
 except ImportError:
     __version__ = "0.0.0" 
 
-__all__ = [
-    "constants",
-    "interp",
-    "layer",
-    "params",
-    "parcel",
-    "thermo",
-    "winds",
-]
+__doc__ = _calc.__doc__
+
+__all__ = [s for s in dir(_calc) if not s.startswith('_')]
 
